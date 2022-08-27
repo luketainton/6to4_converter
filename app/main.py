0577638b82d12665487eb828140ce6062e681a45
Luke Tainton <luke@tainton.uk>
2022-08-27 20:13:21 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 """Get 6to4 address from IPv4 address."""
 
 import argparse
+import sentry_sdk
 
 
 def parse_args():  # pragma: no cover
@@ -52,6 +53,9 @@ def ipv4_to_ipv6(ipv4):
 
 def main():  # pragma: no cover
     """Main function."""
+    
+    sentry_sdk.init("https://14fab091f12c45d299569dcefd1bd716@app.glitchtip.com/1704")
+    
     ipv4 = parse_args().address
     output = ipv4_to_ipv6(ipv4)
     print(output)
